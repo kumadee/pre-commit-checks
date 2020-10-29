70496d696c288c191cc2f59d338d26f42af0297a
kumadee <deepak.kumar@oxid-esales.com>
2020-10-29 16:25:26 +0100

    simplify check

--- pre_commit_hooks/check_string_occurrence.sh
+++ pre_commit_hooks/check_string_occurrence.sh
@@ -1,13 +1,3 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-set -e
-
-fail_if_found() {
-  if [ "$?" == "0" ]; then
-    return 1
-  else
-    return 0
-  fi
-}
-
-grep "$@" /dev/null || fail_if_found
+! grep "$@" /dev/null
